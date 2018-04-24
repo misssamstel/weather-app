3542968fac43d256c9c015002832869f10b3b66d
Sam <sambryony.tack@linecorp.com>
2018-04-24 12:52:25 +0900

    Set-up React, Babel, Webpack

---
+++ src/app.js
@@ -0,0 +1,6 @@
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ import WeatherApp from './components/WeatherApp'
+
+ const app = document.getElementById('app');
+ ReactDOM.render(<WeatherApp />, app);
---
+++ src/components/WeatherApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Weather from './Weather'
+
+export default class WeatherApp extends React.Component {
+  render() {
+    return (
+      <div>
+          <Header />
+          <Weather />
+      </div>
+    )
+  }
+}
