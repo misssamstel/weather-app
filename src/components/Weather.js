f9982d9d0bea466802f7ddfed66fd1ebf4e5e88f
Sam <sambryony.tack@linecorp.com>
2018-04-26 19:46:47 +0900

    Make weather/location information display on screen automatically

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,13 @@ export default class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      locationCode: 0
+      latitude: 0,
+      longitude: 0,
+      error: null,
+      locationCode: 0,
+      locationName: "",
+      weatherDesc: "",
+      temperature: 0
     }
     this.weatherAPI = {
       baseURL: 'http://dataservice.accuweather.com',
@@ -13,10 +19,48 @@ export default class Weather extends React.Component {
       currentWeatherURL: 'currentconditions/v1',
       geopositionSearchURL: 'locations/v1/cities/geoposition/search'
     };
+    this.success = this.success.bind(this);
+    this.error = this.error.bind(this);
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
     this.getCurrentWeather = this.getCurrentWeather.bind(this);
     this.getCurrentLocationCode = this.getCurrentLocationCode.bind(this);
   }
   
+  success = position => {
+    return new Promise((resolve, reject) => {
+      let latitude  = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      this.setState({
+        currentLocation: `Latitude is ${latitude} & Longitude is ${longitude}`,
+        latitude: latitude,
+        longitude: longitude
+      });
+      resolve();
+    });
+  }
+
+  error = () => {
+    this.setState({
+      error: "Unable to retrieve your location"
+    });
+  }
+
+  getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation){
+        this.setState({
+          error: "Geolocation is not supported by your browser"
+        });
+        return;
+      }
+      this.setState({
+        currentLocation: "Loading..."
+      });
+      navigator.geolocation.getCurrentPosition(this.success, this.error);
+      resolve();
+    });
+  }
+
   getCurrentWeather = () => {
     axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.currentWeatherURL}/${this.state.locationCode}`, {
       params: {
@@ -28,29 +72,9 @@ export default class Weather extends React.Component {
       let data = res.data[0];
       let temperature = data.Temperature.Metric.Value;
       let weatherDesc = data.WeatherText;
-      console.log(weatherDesc);
-      console.log(`It is ${temperature}°C`);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
-  getCurrentLocationCode = () => {
-    axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.geopositionSearchURL}`, {
-      params: {
-        apikey: this.weatherAPI.apiKey,
-        q: `${this.props.latitude},${this.props.longitude}`,
-        language: 'ja-jp'
-      }
-    })
-    .then((res) => {
-      let data = res.data;
-      let locationCode = data.Key;
-      let locationName = data.LocalizedName;
-      console.log(locationCode);
-      console.log(`${locationName}にいます`);
       this.setState({
-        locationCode: locationCode
+        weatherDesc: weatherDesc,
+        temperature: temperature
       });
     })
     .catch((err) => {
@@ -58,12 +82,49 @@ export default class Weather extends React.Component {
     })
   }
 
+  getCurrentLocationCode = () => {
+    axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.geopositionSearchURL}`, {
+      params: {
+        apikey: this.weatherAPI.apiKey,
+        q: `${this.state.latitude},${this.state.longitude}`,
+        language: 'ja-jp'
+      }
+    })
+    .then((res) => {
+      let data = res.data;
+      let locationCode = data.Key;
+      let locationName = data.LocalizedName;
+      this.setState({
+        locationCode: locationCode,
+        locationName: locationName
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+    if (prevState.latitude !== this.state.latitude || prevState.longitude !== this.state.longitude) {
+      this.getCurrentLocationCode();
+    }
+    if (prevState.locationCode !== this.state.locationCode) {
+      this.getCurrentWeather();
+    }
+  }
+
   render() {
     return (
       <div>
           <p>WEATHER INFO</p>
-          <p><button onClick={this.getCurrentWeather}>get weather</button></p>
-          <p><button onClick={this.getCurrentLocationCode}>get current location code</button></p>
+          {this.state.error && <p>{this.state.error}</p>}
+          <p>You are in {this.state.locationName}</p>
+          <p>It is {this.state.weatherDesc}</p>
+          <p>It is {this.state.temperature}°C</p>
       </div>
     )
   }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -4,60 +4,11 @@ import Header from './Header'
 import Weather from './Weather'
 
 export default class WeatherApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentLocation: "WeatherApp",
-      latitude: 0,
-      longitude: 0,
-      error: null
-    }
-    this.success = this.success.bind(this);
-    this.error = this.error.bind(this);
-    this.getCurrentLocation = this.getCurrentLocation.bind(this);
-  }
-
-  success = position => {
-    let latitude  = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    this.setState({
-      currentLocation: `Latitude is ${latitude} & Longitude is ${longitude}`,
-      latitude: latitude,
-      longitude: longitude
-    });
-    console.log(this.state.latitude)
-  }
-
-  error = () => {
-    this.setState({
-      error: "Unable to retrieve your location"
-    });
-  }
-
-  getCurrentLocation = () => {
-     if (!navigator.geolocation){
-      this.setState({
-        error: "Geolocation is not supported by your browser"
-      });
-      return;
-    }
-    this.setState({
-      currentLocation: "Loading..."
-    });
-    navigator.geolocation.getCurrentPosition(this.success, this.error);
-  }
-  componentDidMount() {
-    this.getCurrentLocation();
-  }
   render() {
     return (
       <div>
-          <Header currentLocation={this.state.currentLocation}/>
-          <Weather latitude={this.state.latitude} longitude={this.state.longitude}/>
-          <div>
-            <p>{this.state.currentLocation}</p>
-            {this.state.error && <p>{this.state.error}</p>}
-          </div>
+        {/* <Header currentLocation={this.state.currentLocation}/> */}
+        <Weather />
       </div>
     )
   }
