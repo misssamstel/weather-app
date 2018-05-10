1699b8af6101f60d6bdb8e16a7181885a18fa005
Sam <sambryony.tack@linecorp.com>
2018-05-10 15:37:51 +0900

    Set-up Redux inside Weather component

--- README.md
+++ README.md
@@ -19,3 +19,11 @@ yarn run build
 ## History
 * v0 - First Steps
 * v1 - State Management
+
+## Weather API
+### AccuWeather
+https://developer.accuweather.com/
+
+### Limitations
+Only certain APIs available with free package. (Forecast 5-day only)
+Limited to 50 API calls a day.
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,165 +1,204 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import {
+  updateWeatherDescTemp,
+  updateLatLong,
+  updateLocation,
+  addError,
+  clearError
+} from '../actions/actions';
+import { bindActionCreators } from 'redux';
+import store from '../store/weatherStore';
 
-const mapStateToProps = state => {
-  return { temperature: state.temperature };
-};
-
-const ConnectedList = ({ temperature }) => (
-  <ul>
-    {temperature.map(el => (
-      <li key={el.id}>
-        {el.title}
-      </li>
-    ))}
-  </ul>
-);
-
-const List = connect(mapStateToProps)(ConnectedList);
-
-export default List;
-
-// export default class Weather extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       latitude: 0,
-//       longitude: 0,
-//       error: null,
-//       locationCode: 0,
-//       locationName: "",
-//       weatherDesc: "",
-//       temperature: 0,
-//       date: new Date()
-//     };
-//     this.date = {
-//       currentYear: this.state.date.getFullYear(),
-//       currentMonth: this.state.date.getMonth(),
-//       currentDay: this.state.date.getDate()
-//     };
-//     this.weatherAPI = {
-//       baseURL: 'http://dataservice.accuweather.com',
-//       apiKey: 'VLYBIQ48eBl4bYVrrwbsbu1h7GguIaAC',
-//       currentWeatherURL: 'currentconditions/v1',
-//       geopositionSearchURL: 'locations/v1/cities/geoposition/search'
-//     };
-//     this.success = this.success.bind(this);
-//     this.error = this.error.bind(this);
-//     this.getCurrentLocation = this.getCurrentLocation.bind(this);
-//     this.getCurrentWeather = this.getCurrentWeather.bind(this);
-//     this.getCurrentLocationCode = this.getCurrentLocationCode.bind(this);
-//   }
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      date: new Date()
+    };
+    this.date = {
+      currentYear: this.state.date.getFullYear(),
+      currentMonth: this.state.date.getMonth(),
+      currentDay: this.state.date.getDate()
+    };
+    this.weatherAPI = {
+      baseURL: 'http://dataservice.accuweather.com',
+      apiKey: 'VLYBIQ48eBl4bYVrrwbsbu1h7GguIaAC',
+      currentWeatherURL: 'currentconditions/v1',
+      geopositionSearchURL: 'locations/v1/cities/geoposition/search',
+      fiveDayForecastURL: 'forecasts/v1/daily/5day',
+      autocompleteSearchURL: 'locations/v1/cities/autocomplete'
+    };
+    this.success = this.success.bind(this);
+    this.error = this.error.bind(this);
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
+    this.getCurrentWeather = this.getCurrentWeather.bind(this);
+    this.getCurrentLocationCode = this.getCurrentLocationCode.bind(this);
+  }
   
-//   success = position => {
-//     return new Promise((resolve, reject) => {
-//       let latitude  = position.coords.latitude;
-//       let longitude = position.coords.longitude;
-//       this.setState({
-//         currentLocation: `Latitude is ${latitude} & Longitude is ${longitude}`,
-//         latitude: latitude,
-//         longitude: longitude
-//       });
-//       resolve();
-//     });
-//   }
+  success = position => {
+    return new Promise((resolve, reject) => {
+      let latitude  = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      this.setState({
+        currentLocation: `Latitude is ${latitude} & Longitude is ${longitude}`,
+        latitude: latitude,
+        longitude: longitude
+      });
+      store.dispatch(updateLatLong({latitude:latitude, longitude:longitude}));
+      resolve();
+    });
+  }
 
-//   error = () => {
-//     this.setState({
-//       error: "Unable to retrieve your location"
-//     });
-//   }
+  error = () => {
+    this.setState({
+      error: "Unable to retrieve your location"
+    });
+    store.dispatch(addError("Unable to retrieve your location"))
+  }
 
-//   getCurrentLocation = () => {
-//     return new Promise((resolve, reject) => {
-//       if (!navigator.geolocation){
-//         this.setState({
-//           error: "Geolocation is not supported by your browser"
-//         });
-//         return;
-//       }
-//       this.setState({
-//         currentLocation: "Loading..."
-//       });
-//       navigator.geolocation.getCurrentPosition(this.success, this.error);
-//       resolve();
-//     });
-//   }
+  getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation){
+        this.setState({
+          error: "Geolocation is not supported by your browser"
+        });
+        store.dispatch(addError("Geolocation is not supported by your browser"))
+        return;
+      }
+      this.setState({
+        currentLocation: "Loading..."
+      });
+      navigator.geolocation.getCurrentPosition(this.success, this.error);
+      resolve();
+    });
+  }
 
-//   getCurrentWeather = () => {
-//     axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.currentWeatherURL}/${this.state.locationCode}`, {
-//       params: {
-//         apikey: this.weatherAPI.apiKey,
-//         language: 'ja-jp'
-//       }
-//     })
-//     .then((res) => {
-//       let data = res.data[0];
-//       let temperature = data.Temperature.Metric.Value;
-//       let weatherDesc = data.WeatherText;
-//       this.setState({
-//         weatherDesc: weatherDesc,
-//         temperature: temperature
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-//   }
+  getCurrentWeather = () => {
+    axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.currentWeatherURL}/${this.state.locationCode}`, {
+      params: {
+        apikey: this.weatherAPI.apiKey,
+        language: 'ja-jp'
+      }
+    })
+    .then((res) => {
+      let data = res.data[0];
+      let temperature = data.Temperature.Metric.Value;
+      let weatherDesc = data.WeatherText;
+      this.setState({
+        weatherDesc: weatherDesc,
+        temperature: temperature
+      });
+      store.dispatch(updateWeatherDescTemp({weatherDesc: weatherDesc, temperature: temperature}));
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
 
-//   getCurrentLocationCode = () => {
-//     axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.geopositionSearchURL}`, {
-//       params: {
-//         apikey: this.weatherAPI.apiKey,
-//         q: `${this.state.latitude},${this.state.longitude}`,
-//         language: 'ja-jp'
-//       }
-//     })
-//     .then((res) => {
-//       let data = res.data;
-//       let locationCode = data.Key;
-//       let locationName = data.LocalizedName;
-//       this.setState({
-//         locationCode: locationCode,
-//         locationName: locationName
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-//   }
+  // TODO: implement 5 day weather forecast
+  // getFiveDayWeather = () => {
+  //   axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.fiveDayForecastURL}/${this.state.locationCode}`, {
+  //     params: {
+  //       apikey: this.weatherAPI.apiKey,
+  //       language: 'ja-jp',
+  //       metric: true
+  //     }
+  //   })
+  //   .then((res) => {
+  //     let data = res.data;
+  //     data.DailyForecasts.forEach(forecast => {
+  //       store.dispatch(getFiveDayWeather({
+  //         date: forecast.Date,
+  //         temperatureMin: forecast.Temperature.Minimum.Value,
+  //         temperatureMax: forecast.Temperature.Maximum.Value
+  //       }));
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  // }
 
-//   componentDidMount() {
-//     this.getCurrentLocation();
+  // TODO: autocomplete search - only searchable with kanji for ja or english for en - use q for query string
+  // getFiveDayWeather = () => {
+  //   axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.autocompleteSearchURL}`, {
+  //     params: {
+  //       apikey: this.weatherAPI.apiKey,
+  //       language: 'ja-jp',
+  //       q: query
+  //     }
+  //   })
+  //   .then((res) => {
+      
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  // }
 
-//     // setInterval(() => {
-//     //   this.getCurrentLocation();
-//     // }, 5 * 60 * 1000);
-//   }
+  getCurrentLocationCode = () => {
+    axios.get(`${this.weatherAPI.baseURL}/${this.weatherAPI.geopositionSearchURL}`, {
+      params: {
+        apikey: this.weatherAPI.apiKey,
+        q: `${this.state.latitude},${this.state.longitude}`,
+        language: 'ja-jp'
+      }
+    })
+    .then((res) => {
+      let data = res.data;
+      let locationCode = data.Key;
+      let locationName = data.LocalizedName;
+      this.setState({
+        locationCode: locationCode,
+        locationName: locationName
+      });
+      store.dispatch(updateLocation({locationCode:locationCode, locationName:locationName}));
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
 
-//   componentDidUpdate(prevProp, prevState) {
-//     if (prevState.latitude !== this.state.latitude || prevState.longitude !== this.state.longitude) {
-//       this.getCurrentLocationCode();
-//     }
-//     if (prevState.locationCode !== this.state.locationCode) {
-//       this.getCurrentWeather();
-//     }
-//   }
+  componentDidMount() {
+    this.getCurrentLocation();
 
-//   // componentWillUnmount() {
-//   //   clearInterval(this.interval);
-//   // }
+    // setInterval(() => {
+    //   this.getCurrentLocation();
+    // }, 5 * 60 * 1000);
+  }
 
-//   render() {
-//     return (
-//       <div>
-//           <p>WEATHER INFO</p>
-//           {this.state.error && <p>{this.state.error}</p>}
-//           <p>Today is {`${this.date.currentYear}-${this.date.currentMonth + 1}-${this.date.currentDay}`}</p>
-//           <p>You are in {this.state.locationName}</p>
-//           <p>It is {this.state.weatherDesc}</p>
-//           <p>It is {this.state.temperature}°C</p>
-//       </div>
-//     )
-//   }
-// }
+  componentDidUpdate(prevProp, prevState) {
+    if (prevState.latitude !== this.state.latitude || prevState.longitude !== this.state.longitude) {
+      this.getCurrentLocationCode();
+    }
+    if (prevState.locationCode !== this.state.locationCode) {
+      this.getCurrentWeather();
+    }
+  }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
+
+  render() {
+    return (
+      <div>
+          <p>WEATHER INFO</p>
+          {this.props.state.errors && <p>{this.props.state.errors}</p>}
+          <p>Today is {`${this.date.currentYear}-${this.date.currentMonth + 1}-${this.date.currentDay}`}</p>
+          <p>You are in {this.props.state.locationName}</p>
+          <p>It is {this.props.state.weatherDesc}</p>
+          <p>It is {this.props.state.temperature}°C</p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {return {state: state}};
+
+// console.log(store.getState())
+
+export default connect(mapStateToProps)(Weather);
